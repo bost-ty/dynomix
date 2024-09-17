393da68f27a7c94267f392089e361fc77c7b2881
bost-ty <61333914+bost-ty@users.noreply.github.com>
2024-09-17 15:30:59 -0700

    Build UUIDs on mock server launch, not on every request

--- mock.js
+++ mock.js
@@ -3,140 +3,141 @@ import { randomUUID } from "node:crypto";
 
 String.prototype.toNumber = (s) => Number(s);
 
+const data = [
+	{
+		title:
+			"COUNTING-IN_v2_fast_final_finalfinal_usethisone_20240928_final_usethisone.mov",
+		key: randomUUID(),
+	},
+	{
+		title: "Input 2",
+		key: randomUUID(),
+	},
+	{
+		title: "Input 3",
+		key: randomUUID(),
+	},
+	{
+		title: "Input 4",
+		key: randomUUID(),
+	},
+	{
+		title: "Input 5",
+		key: randomUUID(),
+	},
+	{
+		title: "Input 6",
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+	{
+		title: randomUUID().slice(0, 80),
+		key: randomUUID(),
+	},
+];
+
 const server = createServer((req, res) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	if (req.url === "/instance/VMIX_B/data" && req.method === "GET") {
-		const data = [
-			{
-				title:
-					"COUNTING-IN_v2_fast_final_finalfinal_usethisone_20240928_final_usethisone.mov",
-				key: randomUUID(),
-			},
-			{
-				title: "Input 2",
-				key: randomUUID(),
-			},
-			{
-				title: "Input 3",
-				key: randomUUID(),
-			},
-			{
-				title: "Input 4",
-				key: randomUUID(),
-			},
-			{
-				title: "Input 5",
-				key: randomUUID(),
-			},
-			{
-				title: "Input 6",
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-			{
-				title: randomUUID().slice(0, 80),
-				key: randomUUID(),
-			},
-		];
 		res.setHeader("Content-Type", "application/json");
 		res.statusCode = 200;
 		res.end(JSON.stringify(data));
